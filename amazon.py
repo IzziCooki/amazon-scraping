e300cf9f354da3b382dbf60af802237145497600
IzziCooki <63527400+IzziCooki@users.noreply.github.com>
2022-04-08 14:34:29 -0400

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 def scrape_amazon(url):
-    r = requests.get(url)
+ 
 
     r = requests.get(url, headers={
                      "User-Agent": "Chrome OS/Chrome browser: Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36"})
