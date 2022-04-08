fe2874a507e5f9fce3b9437a5f38fd7b702c7fa4
IzziCooki <63527400+IzziCooki@users.noreply.github.com>
2022-04-08 14:34:07 -0400

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -2,8 +2,6 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-url = "https://www.amazon.com/Turtle-Beach-Gaming-Headset-PlayStation/dp/B00ZC3S818/?_encoding=UTF8&pf_rd_p=7ea15e7b-fb3f-4eb6-95b9-585f7ccb8346&pd_rd_wg=szula&pf_rd_r=KT7M9F4RT4RV5RB7XZKG&pd_rd_w=GP3DX&pd_rd_r=03d83078-c27c-4ef1-82d3-e891eb8976f6&ref_=pd_gwm_bmx_gp_8b80hvqo"
-
 
 def scrape_amazon(url):
     r = requests.get(url)
